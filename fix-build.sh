819af84eedc1c7d9b1f0487e7f7ca776b27ed208
markus <markus@75d53db5-b72d-0410-9bdf-f62711b48035>
2009-04-28 02:13:33 +0000

    Fixed deployment for grix.
    git-svn-id: https://projects.arcs.org.au/svn/grix/trunk/grix@302 75d53db5-b72d-0410-9bdf-f62711b48035

---
+++ fix-build.sh
@@ -0,0 +1,11 @@
+cd target
+
+# fix grisu jar file
+mkdir grix
+cd grix
+unzip -o ../grix-1.1-dist.jar
+rm ../grix-1.1-dist.jar
+rm -f META-INF/INDEX.LIST
+cp ../../deployment-config/log4j.properties .
+jar cmf ../../grix_impl/MANIFEST.MF ../grix.jar .
+cd ..
